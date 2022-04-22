db61aa21a0e8dee12653d9069ace9d630e704a45
jexu07 <jexu@ucsd.edu>
2022-04-22 13:33:15 -0700

    fixed test 6
    i checked for an exclamation mark before the open bracket so I would know if it was an image or a link

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -19,6 +19,10 @@ public class MarkdownParse {
         while(currentIndex < markdown.length() && markdown.indexOf("[", currentIndex) != -1){
             int openBracketTemp = markdown.indexOf("[", currentIndex);
             if(openBracketTemp != -1){
+                if(openBracketTemp != 0 && markdown.indexOf("!", openBracketTemp - 1) == openBracketTemp - 1){
+                    currentIndex = openBracketTemp + 1;
+                    continue;
+                }
                 openBracket = openBracketTemp;
             }
             else{
@@ -60,13 +64,31 @@ public class MarkdownParse {
     }
 
 
-    public static void main(String[] args) throws IOException {
+   /**
+   public static void main(String[] args) throws IOException {
+        
         Path fileName = Path.of(args[0]);
-    
         System.out.println(fileName);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
         System.out.println("ha!");
     }
+    */
+
+    public static void main(String[] args) throws IOException {
+        {    Path fileName = Path.of("test-file.md");
+            String content = Files.readString(fileName);
+            ArrayList<String> links = getLinks(content);
+            System.out.println(links);
+        }
+        for(int i = 2; i < 9; i++){
+            Path fileName = Path.of("test-file" + i + ".md");
+            String content = Files.readString(fileName);
+            ArrayList<String> links = getLinks(content);
+            System.out.println(links);
+        }
+       
+    }
+    
 }
